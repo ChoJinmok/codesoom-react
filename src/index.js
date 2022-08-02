c690b3c2ad5fbc4480c504d9e2c99249af06d76b
ChoJinmok <jjmok1127@gmail.com>
2022-08-02 19:30:28 +0900

    JSX - JSX

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
-function createElement(tagName, ...children) {
-  const paragraph = document.createElement(tagName);
+/* @jsx createElement */
 
-  children.forEach((child) => {
-    paragraph.appendChild(child);
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
   });
 
-  return paragraph;
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
 }
 
-//
+let count = 0;
 
-document
-  .getElementById("app")
-  .appendChild(
-    createElement(
-      "div",
-      createElement(
-        "p",
-        ...[1, 2, 3].map((i) => document.createTextNode(`Hello, world! ${i} `))
-      ),
-      createElement("p", document.createTextNode("Hi, world!"))
-    )
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function handleClickNumber(value) {
+  count = value;
+  render();
+}
+
+// DSL (Domain-Specific Languages, 도메인 특화 언어)
+// DSL로 작성 후 JS로 변환(Balbel)
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me! (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
   );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
