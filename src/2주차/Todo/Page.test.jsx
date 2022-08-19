165c3d0ee1d9631c969cdf8293dc97e61618688a
ChoJinmok <jjmok1127@gmail.com>
2022-08-19 19:27:21 +0900

    테스트 작성 중

---
+++ src/2주차/Todo/Page.test.jsx
@@ -0,0 +1,44 @@
+import { render } from '@testing-library/react';
+
+import Page from './Page';
+
+describe('Page component', () => {
+  const handleChangeTitle = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+
+  const setup = ({ taskTitle = '', tasks = [] } = {}) => render(
+    <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />,
+  );
+
+  context('When tasks is empty', () => {
+    const { getByText } = setup();
+
+    expect(getByText('할 일이 없어요!')).toBeTruthy();
+  });
+
+  context('When tasks exist', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '뭐라도 하기',
+      },
+      {
+        id: 2,
+        title: '코드숨 화이팅!',
+      },
+      {
+        id: 3,
+        title: '리뷰 감사합니다!',
+      },
+    ];
+
+    const {} = setup();
+  });
+});
