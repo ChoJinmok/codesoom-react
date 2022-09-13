fe804d6ab9f4f1434553bdadc507b3bd0c4e1397
ChoJinmok <jjmok1127@gmail.com>
2022-09-13 21:36:07 +0900

    react router dom으로 변경

--- src/5주차/App.jsx
+++ src/5주차/App.jsx
@@ -1,19 +1,33 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
+  // const { location: { pathname } } = window;
 
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/about': AboutPage,
+  //   '/restaurants': RestaurantsPage,
+  // }[pathname] || NotFoundPage;
 
   return (
-    <MyComponent />
+    <BrowserRouter>
+      <Routes>
+        {/* 원래 Switch를 사용할 때는 /를 포함하고 있으면 /에 다 걸리게 된데 그래서 exact를 사용해줘야했었다 */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 
   // if (pathname === '/') {
