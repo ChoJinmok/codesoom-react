4a30beae01ce6b24f22bf136cbcdbf84f0c91992
ChoJinmok <jjmok1127@gmail.com>
2022-09-13 18:03:13 +0900

    App에서 객체와 path 활용해서 페이지 접근 할 수 있게 해줌

--- src/5주차/App.jsx
+++ src/5주차/App.jsx
@@ -1,9 +1,18 @@
 import RestaurantsPage from './RestaurantsPage';
 
+function HomePage() {
+  return (
+    <h1>Home</h1>
+  );
+}
+
 export default function App() {
   const { location: { pathname } } = window;
 
-  const MyComponent = RestaurantsPage;
+  const MyComponent = {
+    '/': HomePage,
+    '/retaurants': RestaurantsPage,
+  }[pathname];
 
   return (
     <MyComponent />
