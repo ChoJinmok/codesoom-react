5e3e25ed49009d9945bed7c963f16565df217605
ChoJinmok <jjmok1127@gmail.com>
2022-09-14 20:20:12 +0900

    HomePage Component a태그 Link로 변경 - Link 사용이유 체크 - 테스트 코드 작성 체크

--- src/5주차/HomePage.jsx
+++ src/5주차/HomePage.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+// a태그의 경우 새롭게 통신이 일어난다.
+// Link는 통신이 일어나지 않고 주소만 바꿔준다 -> SPA구현 가능하게 해준다. -> 로딩중 표시도 가능하게 해줌
+// 사용자 경험도 좋고, 실제로 빠르게 일어난다.
+
 export default function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </>
   );
--- src/5주차/HomePage.test.jsx
+++ src/5주차/HomePage.test.jsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
+// Link의 경우 BrowseRouter 안에서만 작동해야하는데 바깥쪽에 있다고 애러가 난다.
+// 하지만 BrowserRouter는 무겁기 때문에 MemoryRouter를 이용하면 가볍게 테스트가 가능하다.
 test('HomePage', () => {
   render((
-    <HomePage />
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
   ));
 });
