a8fc9b6809706c37c47828ce6e5bdffb77342a63
ChoJinmok <jjmok1127@gmail.com>
2022-09-15 23:20:22 +0900

    Red: Home 페이지 테스트 수정

--- src/5주차/HomePage.test.jsx
+++ src/5주차/HomePage.test.jsx
@@ -6,10 +6,20 @@ import HomePage from './HomePage';
 
 // Link의 경우 BrowseRouter 안에서만 작동해야하는데 바깥쪽에 있다고 애러가 난다.
 // 하지만 BrowserRouter는 무겁기 때문에 MemoryRouter를 이용하면 가볍게 테스트가 가능하다.
-test('HomePage', () => {
-  render((
+test('HomePage', async () => {
+  const { container, getAllByRole } = render((
     <MemoryRouter>
       <HomePage />
     </MemoryRouter>
   ));
+
+  const targetPaths = ['about', 'restaurants'];
+
+  const linkList = getAllByRole('link');
+
+  expect(container).toHaveTextContent('Home');
+
+  linkList.forEach((link, index) => {
+    expect(link.href).toContain(targetPaths[index]);
+  });
 });
