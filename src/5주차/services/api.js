fee8763d91e4a685a3e96eda43733f9932188c7c
ChoJinmok <jjmok1127@gmail.com>
2022-09-16 20:06:57 +0900

    Green: 레스토랑 상세 데이터 api fetch 구현

--- src/5주차/services/api.js
+++ src/5주차/services/api.js
@@ -13,3 +13,11 @@ export async function fetchRestaurants({ regionName, categoryId }) {
 
   return restaurants;
 }
+
+export async function fetchRestaurantDetail({ restaurantId }) {
+  const url = `https://eatgo-customer-api.ahastudio.com/restaurants/${restaurantId}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
