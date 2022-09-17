b1fca3f4cf62e13fe89ae5850d1e5689d581649a
ChoJinmok <jjmok1127@gmail.com>
2022-09-17 22:18:52 +0900

    Red: 데이터 받아오기 전 순간적으로 retaurantDetail = null load restaurant detail action 테스트

--- src/5주차/actions.test.js
+++ src/5주차/actions.test.js
@@ -96,7 +96,8 @@ describe('actions', () => {
 
       const actions = store.getActions();
 
-      expect(actions[0]).toEqual(setRestaurantDetail([]));
+      expect(actions[0]).toEqual(setRestaurantDetail(null));
+      expect(actions[1]).toEqual(setRestaurantDetail([]));
     });
   });
 });
