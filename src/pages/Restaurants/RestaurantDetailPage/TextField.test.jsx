e50150b77189e64f8b2b94137895454bf6be04dd
ChoJinmok <jjmok1127@gmail.com>
2022-09-27 17:32:19 +0900

    Red: TextField 테스트 value 가지게끔 테스트 수정

--- src/pages/Restaurants/RestaurantDetailPage/TextField.test.jsx
+++ src/pages/Restaurants/RestaurantDetailPage/TextField.test.jsx
@@ -11,33 +11,33 @@ describe('TextField', () => {
     handleChange.mockClear();
   });
 
-  function renderTextField({ label, type, name }) {
+  function renderTextField({
+    label, type, name, value,
+  }) {
     return render(
       <TextField
         label={label}
         type={type}
         name={name}
+        value={value}
         onChange={handleChange}
       />,
     );
   }
 
-  context('with type', () => {
-    it('renders label and input control', () => {
-      const { queryByLabelText } = renderTextField({
-        label: '평점',
-        type: 'number',
-        name: 'score',
-      });
-
-      expect(queryByLabelText('평점')).not.toBeNull();
+  it('renders label and input control', () => {
+    const { getByLabelText } = renderTextField({
+      label: '평점',
+      value: '5',
     });
 
-    it('renders \'text\'input control', () => {
+    expect(getByLabelText('평점').value).toBe('5');
+  });
+
+  context('with type', () => {
+    it('renders setting input control', () => {
       const { container } = renderTextField({
-        label: '평점',
         type: 'number',
-        name: 'score',
       });
 
       expect(container).toContainHTML('type="number"');
@@ -45,35 +45,24 @@ describe('TextField', () => {
   });
 
   context('without type', () => {
-    it('renders label and input control', () => {
-      const { queryByLabelText } = renderTextField({
-        label: '리뷰 설명',
-        name: 'description',
-      });
-
-      expect(queryByLabelText('리뷰 설명')).not.toBeNull();
-    });
-
-    it('renders \'text\'input control', () => {
+    it('renders \'text\' input control', () => {
       const { container } = renderTextField({
-        label: '리뷰 설명"',
-        name: 'description',
+        label: '평점',
       });
 
       expect(container).toContainHTML('type="text"');
     });
   });
 
-  controls.forEach(({ label, name, value }) => {
-    it('listens change event', () => {
-      const { getByLabelText } = renderTextField({ label, name });
+  it('listens change event', () => {
+    const { label, name, value } = controls[1];
 
-      // not.toBeNull을 사용할 땐 query사용하는 것이 좋다(get은 없으면 그냥 에러가 나기 때문)
-      fireEvent.change(getByLabelText(label), {
-        target: { value },
-      });
+    const { getByLabelText } = renderTextField({ label, name });
 
-      expect(handleChange).toBeCalledWith({ name, value });
-    });
+    fireEvent.change(getByLabelText(label), { target: { value } });
+
+    expect(handleChange).toBeCalledWith({ name, value });
   });
 });
+
+// not.toBeNull을 사용할 땐 query사용하는 것이 좋다(get은 없으면 그냥 에러가 나기 때문)
